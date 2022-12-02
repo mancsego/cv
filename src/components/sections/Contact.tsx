5b099c2bb44ccec4c0556b16fb24391736e07243
István Ábrahám <istvan.abraham@vicampo.de>
2022-12-02 15:20:24 +0100

    Add sections

---
+++ src/components/sections/Contact.tsx
@@ -0,0 +1,7 @@
+const Contact = (): JSX.Element => {
+  return (
+        <div>Contact</div>
+  )
+}
+
+export default Contact
---
+++ src/components/sections/Social.tsx
@@ -0,0 +1,7 @@
+const Social = (): JSX.Element => {
+  return (
+        <div>Social</div>
+  )
+}
+
+export default Social
