03259e18e248bee2904e525544b18cfff11a5b52
István Ábrahám <istvan.abraham@vicampo.de>
2023-01-01 18:01:33 +0100

    Finish contact form & some fixes

--- src/components/atoms/Input.tsx
+++ src/components/atoms/Input.tsx
@@ -3,15 +3,29 @@ import './Input.css'
 
 const Input = ({
   label,
+  onChange,
   id = _createIdFromLabel(label),
   type = 'text',
 }: InputProps): JSX.Element => {
   return (
     <div className="grow relative my-4 mx-1 md:mx-4">
       {type === 'textarea' ? (
-        <textarea name={id} id={id} rows={4} cols={50} placeholder=" " />
+        <textarea
+          name={id}
+          id={id}
+          rows={4}
+          cols={50}
+          placeholder=" "
+          onChange={({ target: { value } }) => onChange(value)}
+        />
       ) : (
-        <input type={type} name={id} id={id} placeholder=" " />
+        <input
+          type={type}
+          name={id}
+          id={id}
+          placeholder=" "
+          onChange={({ target: { value } }) => onChange(value)}
+        />
       )}
       <label htmlFor={id} className="absolute">
         {label}
--- src/components/organisms/Contact.tsx
+++ src/components/organisms/Contact.tsx
@@ -1,6 +1,14 @@
+import { useState } from 'react'
 import Input from '../atoms/Input'
 
 const Contact = (): JSX.Element => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+  const handleClick = () => {
+    window.open(`mailto:${email}?subject=Message from ${name}&body=${message}`)
+  }
   return (
     <div id="contact">
       <div className="heading">
@@ -9,15 +17,23 @@ const Contact = (): JSX.Element => {
       </div>
       <div className="flex justify-center">
         <div className="card">
+          <div className="pt-3 pb-5 flex items-center">
+            <h3>Get in touch with me</h3>
+            <svg className="fill-primary h-6 w-6 ml-3">
+              <use xlinkHref="#message" />
+            </svg>
+          </div>
           <div className="flex">
-            <Input label="name" />
-            <Input label="email" type="email" />
+            <Input label="name" onChange={setName} />
+            <Input label="email" type="email" onChange={setEmail} />
           </div>
           <div>
-            <Input label="message" type="textarea" />
+            <Input label="message" type="textarea" onChange={setMessage} />
           </div>
           <div className="flex justify-end mx-4">
-            <button className="btn btn-primary">Send</button>
+            <button className="btn btn-primary" onClick={handleClick}>
+              Send
+            </button>
           </div>
         </div>
       </div>
