c77f5169fe825ca677e5a6c471ceb76025e4496b
István Ábrahám <istvan.abraham@vicampo.de>
2023-01-01 19:21:01 +0100

    Add github pages config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: '/cv/',
   plugins: [
     VitePWA({
       devOptions: {
@@ -20,17 +21,17 @@ export default defineConfig({
         theme_color: '#242424',
         icons: [
           {
-            src: '/android-chrome-192x192.png',
+            src: 'android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/android-chrome-512x512.png',
+            src: 'android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: '/maskable-icon.png',
+            src: 'maskable-icon.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
