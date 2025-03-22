17698d5ae70a6996c2b7f6cc1c92a3bcd334afa9
Istvan Abraham <mancsego@gmail.com>
2025-03-22 21:00:01 +0100

    Fix for defaulting translation

--- src/hooks/translator.ts
+++ src/hooks/translator.ts
@@ -3,7 +3,7 @@ import { useCmsStore } from '@/store/cms'
 const useTranslations = () => {
   const t = useCmsStore((state) => state.translations)
 
-  return (key: string) => t[key] ?? ''
+  return (key: string) => t[key] ?? key
 }
 
 export { useTranslations }
