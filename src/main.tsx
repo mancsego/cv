83dc843c900cefad6a20c47089ca259761feb404
Istvan Abraham <mancsego@gmail.com>
2025-03-22 10:52:55 +0100

    Simplify main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { store } from './store/index'
+import React, { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <Provider store={store}>
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode>
       <App />
-    </Provider>
-  </React.StrictMode>
+  </StrictMode>
 )
