e82986e6ab56665a8d204237a6bf638343c83ccb
Istvan Abraham <mancsego@gmail.com>
2025-03-23 12:53:09 +0100

    Fix overlay behavior

--- src/components/molecules/Navigation.tsx
+++ src/components/molecules/Navigation.tsx
@@ -24,14 +24,17 @@ const NAV_ITEMS = [
 
 function Navigation() {
   const [open, setOpen] = useState(false)
+  const handler = (open: boolean) => () => {
+    setOpen(open)
+  }
 
   const content = NAV_ITEMS.map(({ link, title }) => (
-    <NavItem title={title} link={link} key={link} onClick={() => setOpen(false)} />
+    <NavItem title={title} link={link} key={link} onClick={handler(false)} />
   ))
   return (
-    <div className="md:hidden absolute bottom-0 left-0 right-0 bg-react-gray text-center border-t-2 border-secondary">
+    <div className="md:hidden absolute bottom-0 left-0 right-0 bg-react-gray text-center border-t-2 border-secondary pointer-events-auto">
       {open && <div className="flex flex-col items-center">{content}</div>}
-      <button onClick={() => setOpen(!open)} className="active:animate-spin py-2">
+      <button onClick={handler(!open)} className="active:animate-spin py-2">
         <img src={menuIcon} alt="menu" width={36} height={34} />
       </button>
     </div>
