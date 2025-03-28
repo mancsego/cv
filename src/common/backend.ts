7cc943d25492aac0d53d09aae5bbf8e22f092404
Istvan Abraham <mancsego@gmail.com>
2025-03-28 01:23:39 +0100

    Use app checks

--- src/common/backend.ts
+++ src/common/backend.ts
@@ -1,9 +1,6 @@
-let cache: null | {
-  initializeApp: CallableFunction
-  getStorage: CallableFunction
-  getDownloadURL: CallableFunction
-  ref: CallableFunction
-} = null
+import { initializeApp } from 'firebase/app'
+import { getToken, initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check'
+import { getDownloadURL, getStorage, ref as storageRef } from 'firebase/storage'
 
 const ALLOWED_LANG = ['en', 'de', 'hu']
 
@@ -14,10 +11,11 @@ const {
   VITE_FIREBASE_PROJECT_ID: projectId,
   VITE_FIREBASE_BUCKET: storageBucket,
   VITE_FIREBASE_MESSAGING: messagingSenderId,
-  VITE_FIREBASE_APP_ID: appId
+  VITE_FIREBASE_APP_ID: appId,
+  VITE_FIREBASE_CAPTCHA_KEY: captchaKey
 } = import.meta.env
 
-const config = {
+const app = initializeApp({
   apiKey,
   authDomain,
   databaseURL,
@@ -25,21 +23,26 @@ const config = {
   storageBucket,
   messagingSenderId,
   appId
-}
+})
+
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider(captchaKey),
+  isTokenAutoRefreshEnabled: true
+})
 
 const fetchDb = async (language: string) => {
   const lang = ALLOWED_LANG.includes(language) ? language : 'en'
+  const { token } = await getToken(appCheck, true)
+
   const url = `https://cv-istvan-abraham-default-rtdb.europe-west1.firebasedatabase.app/${lang}.json`
 
-  const res = await fetch(url)
+  const res = await fetch(url, { headers: { 'X-Firebase-AppCheck': token } })
   return await res.json()
 }
 
 const fetchFile = async () => {
-  const { getStorage, getDownloadURL, ref, initializeApp } = await _getDependencies()
-  initializeApp(config)
   const storage = getStorage()
-  const resource = ref(storage, `downloads/CV_${getLang()}.pdf`)
+  const resource = storageRef(storage, `downloads/CV_${getLang()}.pdf`)
 
   window.open(await getDownloadURL(resource))
 }
@@ -47,15 +50,6 @@ const fetchFile = async () => {
 const getLang = () => {
   const searchParams = new URLSearchParams(location.search)
   return searchParams.get('lang') ?? 'en'
-}
-
-const _getDependencies = async () => {
-  if (cache === null) {
-    const { getStorage, getDownloadURL, ref } = await import('firebase/storage')
-    const { initializeApp } = await import('firebase/app')
-    cache = { getStorage, getDownloadURL, ref, initializeApp }
-  }
-  return cache
 }
 
 export { fetchDb, fetchFile, getLang }
